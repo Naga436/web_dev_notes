bd03354b4c2a43b6ae9a8493b2a3e25ec3b61ec2
Naga <padalanagalakshmi79416@gmail.com>
2025-07-17 00:02:39 +0530

    hyugyug

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "1 2 3 4 5 7 8 wiefji4jf3ofj3ifm3oj3p " << std::endl;
+    std::cout << "1 2 3 4 5 7 8 wiefji4jf3ofj3ifm3oj3p 6 8 9 " << std::endl;
     return 0;
 }
 
